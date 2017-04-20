f3abd06027e130d102700da11f2b4e8a2ce939eb
Ihar Yachnik <jutigor@yandex.ru>
2017-04-20 22:56:56 +0300

    Change markup to HomeCategoryHeader

--- src/views/home/home.jsx
+++ src/views/home/home.jsx
@@ -1,10 +1,7 @@
-import { Link } from 'react-router';
-
 import Layout from 'components/layout/layout';
-import Button from 'components/button/button';
-import CategoryHeader from 'components/category-header/category-header';
+import HomeCategoryHeader from 'views/home/category-header/home-category-header';
 
-import { CATEGORY_CONSTANTS_ENUM, CATEGORY_CONSTANTS } from 'config/category.constants';
+import { CATEGORY_CONSTANTS_ENUM } from 'config/category.constants';
 
 import './home.scss';
 
@@ -12,38 +9,11 @@ const home = () => {
   return (
     <Layout>
       <div className='home'>
-        <CategoryHeader
-          category={CATEGORY_CONSTANTS_ENUM.MENS_OUTERWEAR}
-        >
-          <Link to={CATEGORY_CONSTANTS.MENS_OUTERWEAR}>
-            <Button title='Shop now' />
-          </Link>
-        </CategoryHeader>
-
-        <CategoryHeader
-          category={CATEGORY_CONSTANTS_ENUM.LADIES_OUTERWEAR}
-        >
-          <Link to={CATEGORY_CONSTANTS.LADIES_OUTERWEAR}>
-            <Button title='Shop now' />
-          </Link>
-        </CategoryHeader>
-
+        <HomeCategoryHeader category={CATEGORY_CONSTANTS_ENUM.MENS_OUTERWEAR} />
+        <HomeCategoryHeader category={CATEGORY_CONSTANTS_ENUM.LADIES_OUTERWEAR} />
         <div className='home__category-container'>
-          <CategoryHeader
-            category={CATEGORY_CONSTANTS_ENUM.MENS_TSHIRTS}
-          >
-            <Link to={CATEGORY_CONSTANTS.MENS_TSHIRTS}>
-              <Button title='Shop now' />
-            </Link>
-          </CategoryHeader>
-
-          <CategoryHeader
-            category={CATEGORY_CONSTANTS_ENUM.LADIES_TSHIRTS}
-          >
-            <Link to={CATEGORY_CONSTANTS.LADIES_TSHIRTS}>
-              <Button title='Shop now' />
-            </Link>
-          </CategoryHeader>
+          <HomeCategoryHeader category={CATEGORY_CONSTANTS_ENUM.MENS_TSHIRTS} />
+          <HomeCategoryHeader category={CATEGORY_CONSTANTS_ENUM.LADIES_TSHIRTS} />
         </div>
       </div>
     </Layout >
